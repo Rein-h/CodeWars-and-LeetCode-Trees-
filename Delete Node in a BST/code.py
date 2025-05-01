0b14c0965da850754b2ff6b76b3b4eafd4caf314
Rein-h <pavlunyshyn.pn@ucu.edu.ua>
2025-05-01 12:09:09 +0300

    Second task added

--- Delete Node in a BST/code.py
+++ Delete Node in a BST/code.py
@@ -1,2 +1,24 @@
 """Delete Node in a BST"""
 
+def tree_by_levels(node):
+    if not node:
+        return []
+
+    result = [node.value]
+    queue = [node]
+    
+    while True:
+        q = []
+        for node in queue:
+            if node.left:
+                result.append(node.left.value)
+                q.append(node.left)
+            if node.right:
+                result.append(node.right.value)
+                q.append(node.right)
+
+        queue = q
+        if not queue:
+            break
+    
+    return result
